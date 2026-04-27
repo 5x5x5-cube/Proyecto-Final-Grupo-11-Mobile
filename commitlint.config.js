e8175721872c6845857ef43e933a107d6f4dad02
kirhammer <andres.pena@treinta.co>
2026-04-26 19:18:01 -0500

    Revert "chore: disable commitlint subject-case rule"
    This reverts commit 467523e1fd7482a0d9cd6e431e0b6d66d9215a42.

--- commitlint.config.js
+++ commitlint.config.js
@@ -8,6 +8,5 @@ module.exports = {
       ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert', 'ci'],
     ],
     'subject-max-length': [2, 'always', 100],
-    'subject-case': [0], // Disable subject-case rule
   },
 };
